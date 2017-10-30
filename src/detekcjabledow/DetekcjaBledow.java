47e085834e075da18aa9d515180051e61b8aadde
erdePL <erde@op.pl>
2017-10-30 12:16:04 +0100

    Initial Commit

---
+++ src/detekcjabledow/DetekcjaBledow.java
@@ -0,0 +1,11 @@
+package detekcjabledow;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class DetekcjaBledow {
+
+    public static void main(String[] args) {     
+        MainWindow program = new MainWindow();
+    }
+}
