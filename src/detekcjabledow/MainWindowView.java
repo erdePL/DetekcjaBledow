1f786de326dbfffddbf8605da675de1217a16257
erdePL <erde@op.pl>
2018-02-25 10:36:09 +0100

    Working GridBagLayout

--- src/detekcjabledow/MainWindowView.java
+++ src/detekcjabledow/MainWindowView.java
@@ -18,6 +18,8 @@ MainWindowView(){  //OPERACJE INICJALIZACJI OKNA
     super("Detekcja Błędów");
     Container okno = getContentPane();
     okno.setLayout(new GridBagLayout());
+    GridBagConstraints c = new GridBagConstraints();
+    c.insets = new Insets(2,3,2,3);
 //--------------------------------------------------------------------------------------------------------
     ArrayList<JLabel> listOfLabels = new ArrayList<>(); //DODAWANIE ETYKIET
     JLabel etykietaWyslanyZnak = new JLabel("Wysłany znak: ");listOfLabels.add(etykietaWyslanyZnak);     
@@ -34,59 +36,57 @@ MainWindowView(){  //OPERACJE INICJALIZACJI OKNA
     JLabel etykietaWykryteBledy = new JLabel("Wykryte Błędy:");listOfLabels.add(etykietaWykryteBledy);   
     JLabel etykietaPoprawionyBinarny = new JLabel("Poprawiony Binarny:");listOfLabels.add(etykietaPoprawionyBinarny);    
     JLabel etykietaOdebranyCiagZnakow = new JLabel("Odebrany Znak:");listOfLabels.add(etykietaOdebranyCiagZnakow);
-    {int distanceFromNorthOfTheWindow = 10;
-        for(JLabel element : listOfLabels){
-            layoutSprezynowy.putConstraint(SpringLayout.WEST, element, 10, SpringLayout.WEST, okno);
-            layoutSprezynowy.putConstraint(SpringLayout.NORTH, element, distanceFromNorthOfTheWindow, SpringLayout.NORTH, okno);
-            okno.add(element);
-            element.setPreferredSize(new Dimension(400, 20));
 
-            distanceFromNorthOfTheWindow += 25;
-        }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    int index = 0;
+    for(JLabel element : listOfLabels){
+        if(index==0)
+            c.anchor = GridBagConstraints.WEST;
+        c.gridy = index++;
+        okno.add(element,c);
     }
 //--------------------------------------------------------------------------------------------------------
     ArrayList<JTextField> listOfDataControlTextFields = new ArrayList();//DODAWANIE PÓL TEKSTOWYCH
     ArrayList<JTextField> listOfTextFields = new ArrayList();
     JTextField poleTekstoweWyslanyZnak = new JTextField();listOfTextFields.add(poleTekstoweWyslanyZnak); poleTekstoweWyslanyZnak.setText(null); poleTekstoweWyslanyZnak.setDocument(new LengthRestrictedDocument(1));
-    JTextField poleTekstoweWyslanyBinarny = new JTextField();listOfTextFields.add(poleTekstoweWyslanyBinarny);  
-    JTextField poleTekstoweBitParzystosci = new JTextField();listOfTextFields.add(poleTekstoweBitParzystosci);         listOfDataControlTextFields.add(poleTekstoweBitParzystosci);    
+    JTextField poleTekstoweWyslanyBinarny = new JTextField();listOfTextFields.add(poleTekstoweWyslanyBinarny);
+    JTextField poleTekstoweBitParzystosci = new JTextField();listOfTextFields.add(poleTekstoweBitParzystosci);         listOfDataControlTextFields.add(poleTekstoweBitParzystosci);
     JTextField poleTekstoweBityHaminga = new JTextField();listOfTextFields.add(poleTekstoweBityHaminga);               listOfDataControlTextFields.add(poleTekstoweBityHaminga);
     JTextField poleTekstoweWielomianCRC16 = new JTextField();listOfTextFields.add(poleTekstoweWielomianCRC16);         listOfDataControlTextFields.add(poleTekstoweWielomianCRC16);
     JTextField poleTekstoweWielomianCRC32 = new JTextField();listOfTextFields.add(poleTekstoweWielomianCRC32);         listOfDataControlTextFields.add(poleTekstoweWielomianCRC32);
     JTextField poleTekstoweWielomianCRCITU = new JTextField();listOfTextFields.add(poleTekstoweWielomianCRCITU);       listOfDataControlTextFields.add(poleTekstoweWielomianCRCITU);
     JTextField poleTekstoweWielomianSDLC = new JTextField();listOfTextFields.add(poleTekstoweWielomianSDLC );          listOfDataControlTextFields.add(poleTekstoweWielomianSDLC);
-    JTextField poleTekstowePrzesylanyCiag = new JTextField();listOfTextFields.add(poleTekstowePrzesylanyCiag); 
+    JTextField poleTekstowePrzesylanyCiag = new JTextField();listOfTextFields.add(poleTekstowePrzesylanyCiag);
     JTextField poleTekstoweDoZaklocenia = new JTextField();listOfTextFields.add(poleTekstoweDoZaklocenia);
-    JTextField poleTekstoweOdebranyCiag = new JTextField();listOfTextFields.add(poleTekstoweOdebranyCiag); 
-    JTextField poleTekstoweWykryteBledy = new JTextField();listOfTextFields.add(poleTekstoweWykryteBledy);   
-    JTextField poleTekstowePoprawionyBinarny = new JTextField();listOfTextFields.add(poleTekstowePoprawionyBinarny); 
-    JTextField poleTekstoweOdebranyZnak = new JTextField(); listOfTextFields.add(poleTekstoweOdebranyZnak);  
-    {int distanceFromNorthOfTheWindow = 10;
-        for(JTextField element : listOfTextFields){
-            layoutSprezynowy.putConstraint(SpringLayout.WEST, element, 150, SpringLayout.WEST, okno);
-            layoutSprezynowy.putConstraint(SpringLayout.NORTH, element, distanceFromNorthOfTheWindow, SpringLayout.NORTH, okno);
-            okno.add(element);
-            element.setPreferredSize(new Dimension(400, 20)); 
-            element.setEditable(false);
+    JTextField poleTekstoweOdebranyCiag = new JTextField();listOfTextFields.add(poleTekstoweOdebranyCiag);
+    JTextField poleTekstoweWykryteBledy = new JTextField();listOfTextFields.add(poleTekstoweWykryteBledy);
+    JTextField poleTekstowePoprawionyBinarny = new JTextField();listOfTextFields.add(poleTekstowePoprawionyBinarny);
+    JTextField poleTekstoweOdebranyZnak = new JTextField(); listOfTextFields.add(poleTekstoweOdebranyZnak);
 
-            distanceFromNorthOfTheWindow += 25;
-        }
+    index = 0;
+    c.gridx = 2;
+    c.weightx = 1;
+    for(JTextField element : listOfTextFields){
+        c.gridy = index++;
+        okno.add(element,c);
+        element.setPreferredSize(new Dimension(400, 20));
+        element.setEditable(false);
     }
 //--------------------------------------------------------------------------------------------------------
     ArrayList<JRadioButton> listOfJRadioButtons = new ArrayList();//DODAWANIE RADIO BUTTONOW....
     ButtonGroup grupaRadiowa = new ButtonGroup();
+    index = 1;
+    c.gridx = 1;
+    c.weightx = 0;
     for(Integer i = 0;  i<6; i++){
         JRadioButton roboczyRadioButton = new JRadioButton();
         grupaRadiowa.add(roboczyRadioButton);
         listOfJRadioButtons.add(roboczyRadioButton);
-        
-        layoutSprezynowy.putConstraint(SpringLayout.EAST, roboczyRadioButton, -5, SpringLayout.WEST, listOfDataControlTextFields.get(i) );
-        layoutSprezynowy.putConstraint(SpringLayout.NORTH, roboczyRadioButton, 0, SpringLayout.NORTH, listOfDataControlTextFields.get(i) );
-        
         roboczyRadioButton.setName(i.toString());
         if(i<2)
         roboczyRadioButton.setEnabled(false);
-        okno.add(roboczyRadioButton);
+        c.gridy = index++;
+        okno.add(roboczyRadioButton,c);
     }
 
 controller = new MainWindowController(listOfTextFields, grupaRadiowa, listOfJRadioButtons, okno);
@@ -114,10 +114,19 @@ controller = new MainWindowController(listOfTextFields, grupaRadiowa, listOfJRad
     //DO TEGO POLA PRZYPISANA JEST GŁÓWNA LOGIKA APLIKACJI
     poleTekstoweDoZaklocenia.addActionListener(ae -> controller.doZakloceniaActionPerformed());
 //--------------------------------------------------------------------------------------------------------
+
+    JLabel bottomStretchBlocker = new JLabel();
+    c.weighty = 1;
+    c.weightx = 0;
+    c.gridy = 14;
+    c.gridx=0;
+    c.fill = GridBagConstraints.NONE;
+    okno.add(bottomStretchBlocker, c);
+
     this.pack();
     this.setVisible(true);
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    this.setResizable(false);
+    //this.setResizable(false);
     poleTekstoweWyslanyZnak.setEditable(true);
     this.setSize(600, 430);
     this.setLocationRelativeTo(null);
